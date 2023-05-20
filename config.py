3c0801c944aace9bd7714e4ac55258bc82250360
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2023-05-21 01:00:57 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,67 @@
+
+
+TOKEN = '6199936043:AAEjHjjmUJ_DWZueyDzXC4Bo6vhjsiOqvzA'
+
+
+
+STARTMSG = "Hi, i am translator bot. I use python and google API. "
+CHOOSEMSG = "Choose language"
+
+
+LANGDICT = {
+
+    'sq': 'albanian',
+    'ar': 'arabic',
+    'be': 'belarusian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'zh-CN': 'chinese_simplified',
+    'zh-TW': 'chinese_traditional',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'gl': 'galician',
+    'de': 'german',
+    'el': 'greek',
+    'iw': 'hebrew',
+    'hi': 'hindi',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'ko': 'korean',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'mk': 'macedonian',
+    'ms': 'malay',
+    'mt': 'maltese',
+    'no': 'norwegian',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sr': 'serbian',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'es': 'spanish',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'yi': 'yiddish',
+  }
+LANGUES = LANGDICT.keys()
